baeb62ce5d1bdac5d54833b8cc9125bbb8f7f7cf
Jonas <61836881+Jonstep101010@users.noreply.github.com>
2023-03-29 09:29:17 +0000

    ft_split prototype

--- ft_split.c
+++ ft_split.c
@@ -12,6 +12,8 @@
 
 #include "libft.h"
 
+/*
+** count words by returning times while loop for *s != c is called*/
 static int	word_counter(char const *s, char c)
 {
 	int	count;
@@ -36,44 +38,28 @@ static int	word_length(char const *s, char c)
 	int	length;
 
 	length = 0;
-	while ((*s) && (*s != c))
-	{
+	while ((s[length]) && (s[length] != c))
 		length++;
-		s++;
-	}
 	return (length);
 }
 
-/**
-** @brief 
-** @param s string to be split
-** @param c delimiter char
-** @return array of new strings - resulting from split,
-** \return NULL if allocation fails
-** @details allocates, returns array of strings (by splitting s using c);
-** \details array has to end with a NULL pointer; use malloc, free
-** \details handle leading & trailing delimiting characters - no empty substrings
-** \details allocated memory freed, no manipulation of input 
-** \details (edge: empty input, only delimiter, no delimiter: return array input)
-*/
 char	**ft_split(char const *s, char c)
 {
-	char	**arr_s;
-	int		count;
+	char	**arr;
 	int		i;
 
 	if (!s)
 		return (0);
 	i = 0;
-	arr_s = malloc(sizeof(char *) * (word_counter(s, c) + 1));
-	if (!arr_s)
+	arr = malloc(sizeof(char *) * (word_counter(s, c) + 1));
+	if (!arr)
 		return (0);
 	while (*s)
 	{
 		if (*s != c)
 		{
-			arr_s[i] = ft_substr(s, 0, word_length(s, c));
-			if (!arr_s[i])
+			arr[i] = ft_substr(s, 0, word_length(s, c));
+			if (!arr[i])
 				return (NULL);
 			i++;
 			s += word_length(s, c);
@@ -81,8 +67,8 @@ char	**ft_split(char const *s, char c)
 		else
 			s++;
 	}
-	arr_s[i] = NULL;
-	return (arr_s);
+	arr[i] = NULL;
+	return (arr);
 }
 
 //determine how many strings to store
