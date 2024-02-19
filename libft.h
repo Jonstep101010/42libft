88f5610bed8e44af948c7c8aca08fed31420d9ec
jschwabe <mail@jschwabe.site>
2024-02-19 19:40:28 +0100

    Refactor Makefile and delete unused list functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: jschwabe <jschwabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 16:26:52 by jschwabe          #+#    #+#             */
-/*   Updated: 2023/05/30 20:52:57 by jschwabe         ###   ########.fr       */
+/*   Updated: 2024/02/19 19:39:21 by jschwabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,6 @@
 # include <stdint.h>
 # include <stddef.h>
 # include <unistd.h>
-
-/*
-** @brief A structure to represent a singly linked list.
-** 
-*/
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}					t_list;
 
 //character tests & conversions
 int			ft_isalpha(int c);
@@ -71,16 +61,4 @@ void		ft_putchar_fd(char c, int fd);
 void		ft_putstr_fd(char *s, int fd);
 void		ft_putendl_fd(char *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
-
-//lists - BONUS
-t_list		*ft_lstnew(void *content);
-void		ft_lstadd_front(t_list **lst, t_list *node);
-int			ft_lstsize(t_list *lst);
-t_list		*ft_lstlast(t_list *lst);
-void		ft_lstadd_back(t_list **lst, t_list *node);
-void		ft_lstdelone(t_list *lst, void (*del)(void *));
-void		ft_lstclear(t_list **lst, void (*del)(void *));
-void		ft_lstiter(t_list *lst, void (*f)(void *));
-t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-
 #endif
